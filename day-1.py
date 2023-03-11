84641fc12906bcbee5d4f88976685ae9798118a3
muharremdemirel <91542346+muharremdemirel@users.noreply.github.com>
2023-03-11 14:06:58 +0300

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,19 @@
+#string (str) --> Metinsel veri tipini temsil eder.
+#integer (int ) --> Sayısal veri tipidir. Ve de tam sayıları temsil eder.
+#float (float) --> Sayısal veri tipini temsil eder.Ondalıklı sayılar bu tipe örnektir.
+#boolean (bool) --> Mantıksal veri tipidir.True veya False değerini alabilmektedir.
+
+#Kodlama.io sitesinde özelikle string veri tipinin kullanıldığını görüyoruz .
+# Arama çubuklarında aramak istediğimiz kursun karşılığında çıkan sonuç  buna örnektir.
+#Bunun yanında diğer veri tiplerine dair örnekler de mevcutur.
+#Örneğin kurs sonunda sertifikaya hak kazanma durumunu sorgulayan boolean veri tipleri bir diğer örnektir. 
+
+#Kategori kısmında veya Eğitmen seçeneğininde seçim kısmında seçtiğimiz şartlara göre önümüze daraltılmış bir sonuç çıkartır.
+# Örneğin -->> Eğer Eğitmen:"Halit Enes Kalaycı" seçeneğine  tıklarsan sadece Halit Enes Kalaycı'nın sunduğu kurslara erişirsin.
+
+if 12<10:
+    print("Eğitim başlamadı")
+elif 7==4:
+    print("Eğitime daha var")
+else:
+    print("Eğitim başladı.")
